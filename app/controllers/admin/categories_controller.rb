9b3693d34489368ab9ee4d7a5f6840718dc55fb2
Porson <porsonlee@gmail.com>
2020-02-06 21:49:18 +0000

    created new admin feature for categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,18 +1,32 @@
 class Admin::CategoriesController < ApplicationController
+  
   http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
 
   def index
-    @category = Category.order(id: :desc).all
-  end
-
-  def create
+    @categories = Category.order(id: :desc).all
   end
 
   def new
+    @category = Category.new
   end
 
-  def destroy
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
   end
   
+  private
+
+  def category_params
+    params.require(:categories).permit(
+      :name,
+    )
+  end
+
 end
 
