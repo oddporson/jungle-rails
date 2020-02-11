2f36e18b44075482df07c2e1a5ac9555bbc2de29
Porson <porsonlee@gmail.com>
2020-02-11 04:26:04 +0000

    validate im the user model actually fixed the issue on passing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  describe 'Validation' do
+    before do
+      @user = User.new(name:"Bruce Wayne", email:"brucewayne@justiceleague.com", password:"ihatesuperman", password_confirmation:"ihatesuperman")
+    end
+
+    it "should have a name" do
+      @user.name = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to_not be_empty
+    end
+
+    it "should have an email" do
+      @user.email = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
+
+    it "should have password" do
+      @user.password = nil
+      @user.password_confirmation = nil
+      @user.valid?
+      expect(@user).to_not be_valid
+    end
+
+    it "should match both passwords to match" do
+      @user.password = "ihatesuperman"
+      @user.password_confirmation = "iHatesupErm@n"
+      @user.valid?
+      expect(@user.errors.full_messages).to include ("Password confirmation doesn't match Password")
+    end
+
+    
+
+    end
+  end
