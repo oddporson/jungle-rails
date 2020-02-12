29d1bdf029934af123f235701a80a2ffe5db9361
Porson <porsonlee@gmail.com>
2020-02-12 01:03:37 +0000

    tested to make sure visitors can navigate to product details

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.feature "Visitors navigates to product details page from homepage", type: :feature, js: true do
+
+  #SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    @category.products.create!(
+      name:  Faker::Hipster.sentence(3),
+      description: Faker::Hipster.paragraph(4),
+      image: open_asset('apparel1.jpg'),
+      quantity: 10,
+      price: 64.99
+    )
+  end
+
+
+    scenario "They see product detail" do
+      #ACT
+      visit root_path
+
+      body = page.find('.product')
+      body.find('header a').click
+      page.find('.products-show')
+
+      #DEBUG
+      save_screenshot
+
+      #VERIFY
+      expect(page).to have_css '.products-show'
+    end
+
+
+
+
+
+end
